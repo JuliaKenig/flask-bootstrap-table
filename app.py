76ab018aad1772ad3f870f45f5862c1dffc28ead
JuliaKenig <goncharova.julietta@gmail.com>
2025-05-11 20:47:46 -0700

    updated

--- app.py
+++ app.py
@@ -6,43 +6,39 @@ A example for creating a Table that is sortable by its header
 """
 
 app = Flask(__name__)
-data = [{
-  "name": "bootstrap-table",
-  "commits": "10",
-  "attention": "122",
-  "uneven": "An extended Bootstrap table"
-},
- {
-  "name": "multiple-select",
-  "commits": "288",
-  "attention": "20",
-  "uneven": "A jQuery plugin"
-}, {
-  "name": "Testing",
-  "commits": "340",
-  "attention": "20",
-  "uneven": "For test"
-}]
+data = [
+  {
+    "name": "Python",
+    "commits": "3000",
+    "uneven": "Popular for data science"
+  },
+  {
+    "name": "JavaScript",
+    "commits": "4200",
+    "uneven": "Used for web development"
+  },
+  {
+    "name": "SQL",
+    "commits": "1500",
+    "uneven": "Great for databases"
+  }
+]
+
 # other column settings -> http://bootstrap-table.wenzhixin.net.cn/documentation/#column-options
 columns = [
   {
-    "field": "name", # which is the field's name of data key 
-    "title": "name", # display as the table header's name
+    "field": "name",
+    "title": "Language",
     "sortable": True,
   },
   {
     "field": "commits",
-    "title": "commits",
-    "sortable": True,
-  },
-  {
-    "field": "attention",
-    "title": "attention",
+    "title": "GitHub Commits",
     "sortable": True,
   },
   {
     "field": "uneven",
-    "title": "uneven",
+    "title": "Description",
     "sortable": True,
   }
 ]
